77bb01b88a6919352b9bf0134b44d8f79f97b8bd
MonroeMegalonyx <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-02-19 14:35:15 -0500

    Merged main

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -2,7 +2,10 @@ import {LogOutButton} from "./auth/LogoutButton.js";
 // Import the functions to list all a user's articles in the dashboard and add a save button for new articles
 import { ListArticles } from "./articles/ArticleListView.js";
 import { NewArticleButton } from "./articles/ArticleSaveForm.js";
-
+// Import the functions to list tasks in dashboard
+import { TaskList } from "./tasks/TaskList.js"
+import { TaskForm } from "./tasks/TaskForm.js"
+import { newTaskButton } from "./tasks/TaskForm.js"
 
 export const Nutshell = () => {
     
@@ -11,18 +14,8 @@ export const Nutshell = () => {
     // Article components
     ListArticles()
     NewArticleButton()
+    // Task components
+    TaskForm()
+    TaskList()
+    newTaskButton()
 }
-import {LogOutButton} from "./auth/LogoutButton.js"
-import { TaskList } from "./tasks/TaskList.js"
-import { TaskForm } from "./tasks/TaskForm.js"
-import { newTaskButton } from "./tasks/TaskForm.js"
-
-export const Nutshell = () => {
-  TaskForm()
-  TaskList()
-  newTaskButton()
-    LogOutButton()
-      // Render all your UI components here
-}
-
-
