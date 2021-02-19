3b3248f083c7c33e9589f3ceaa723b85e90c81bd
MonroeMegalonyx <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-02-19 14:17:40 -0500

    navbar edits

--- src/scripts/nav/NavView.js
+++ src/scripts/nav/NavView.js
@@ -1,7 +1,6 @@
 /*
  *  This is a list component that lists all the navbar entries
  *  after a user has logged in to their dashboard.
- *  It imports the modules 
  * 
  *  Written by Vincent OLeary
  */
@@ -9,7 +8,7 @@
 // Define DOM element the navbar resides in
 const navTarget = document.querySelector("nav")
 
-// Build a button that will show the save form when clicked by the user
+// Build the navbar when the user logs in or sees the splash page
 export const PrintNavbar = () => { 
     navTarget.innerHTML = 
     `   
