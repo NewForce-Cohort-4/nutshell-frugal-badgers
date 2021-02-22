fb487e2ba7c7e00cf9e722836d8b5973c8c6b9c6
Sophia <soapsstone@gmail.com>
2021-02-22 14:12:51 -0500

    css changes

--- src/scripts/tasks/TaskSelect.js
+++ src/scripts/tasks/TaskSelect.js
@@ -1,7 +1,5 @@
 const eventHub = document.querySelector("body")
 
-
-
 document.querySelector("body").addEventListener("click", clickEvent => {
     if(clickEvent.target.id === "#newButtonContainer") {
         TaskForm()
