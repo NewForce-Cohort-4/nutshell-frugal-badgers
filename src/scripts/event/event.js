b01d2b4b023bde139024bcd289fae18f25ceab77
Thomas Frazier <tfrazier2424@gmail.com>
2021-02-22 09:34:18 -0500

    fixed undefined issue when printing

--- src/scripts/event/event.js
+++ src/scripts/event/event.js
@@ -2,7 +2,7 @@ export const individualEvent = (event) => {
     return `
     <div id="event--${event.id}" class="eventCard">
     <p>${event.date}</p>
-    <p>${event.name}</p>
+    <p>${event.event}</p>
     <p>${event.location}</p>
     </div>
     `
